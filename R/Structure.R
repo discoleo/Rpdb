423dab00519fb98386ac05c0b9bf57b442011029
discoleo <leo.mada@syonic.eu>
2026-04-25 02:32:18 +0300

    more work on Sheets;

--- R/Structure.R
+++ R/Structure.R
@@ -13,6 +13,13 @@ as.pdb.structure = function(helix = NULL, sheet = NULL, ...) {
 	return(x);
 }
 
+# Note:
+# - Positions are coded using 4 digits;
+# - Duplicate positions are disambiguated using the Insrtion Code;
+# - Insertion codes use letters: potential for 26*2 more positions;
+# - Sense of Strand:
+#   0 = First strand;
+#   1 = Parallel to previous, -1 = Anti-parallel to previous;
 extract.pdb.structure = function(x, recname) {
 	### Helix:
 	idHelix = which(recname == "HELIX ");
@@ -34,26 +41,45 @@ extract.pdb.structure = function(x, recname) {
 			AAE = hAAE, idAAE = as.integer(hAA2));
 	}
 	### Sheet:
+	# https://www.wwpdb.org/documentation/file-format-content/format33/sect5.html#SHEET
 	idSheet = which(recname == "SHEET ");
 	isSheet = length(idSheet) > 0;
 	sheet   = NULL;
 	if(isSheet) {
 		sSh = x[idSheet];
-		sID   = trim(substr(sSh,  7, 10));
-		sName = trim(substr(sSh, 11, 16));
-		sAAS  = trim(substr(sSh, 17, 20));
-		sChS  = trim(substr(sSh, 21, 22));
-		sAA1  = trim(substr(sSh, 23, 26));
-		sAAE  = trim(substr(sSh, 28, 31)); # 1 Space before?
-		sChE  = trim(substr(sSh, 32, 33));
-		sAA2  = trim(substr(sSh, 34, 37)); # 1 Space after?
-		sType = trim(substr(sSh, 38, 40));
+		sID   = trim(substr(sSh,  8, 10)); # Strand ID
+		sName = trim(substr(sSh, 12, 14)); # Sheet ID
+		sNStr = trim(substr(sSh, 15, 16)); # No. Strands
+		# Start:
+		sAAS  = trim(substr(sSh, 18, 20)); # AA Code
+		sChS  = trim(substr(sSh, 22, 22)); # Chain ID
+		sAASn = trim(substr(sSh, 23, 26)); # Seq No.
+		sAASI = trim(substr(sSh, 27, 27)); # Insertion Code
+		# End:
+		sAAE  = trim(substr(sSh, 29, 31)); # AA Code
+		sChE  = trim(substr(sSh, 32, 33)); # Chain ID
+		sAAEn = trim(substr(sSh, 34, 37)); # Seq No
+		sAAEI = trim(substr(sSh, 38, 38)); # Insertion Code
+		sType = trim(substr(sSh, 39, 40)); # Sense of Strand
+		### Registration
+		# Current Strand:
+		atomRc   = trim(substr(sSh, 42, 45)); # Atom
+		insertRc = trim(substr(sSh, 55, 55)); # Insertion
+		# Previous Strand:
+		atomRp   = trim(substr(sSh, 42, 45)); # Atom
+		insertRp = trim(substr(sSh, 70, 70)); # Insertion
 		# TODO: remaining fields;
 		sheet = data.frame(ID = as.integer(sID), Name  = sName,
-			Type = as.integer(sType),
+			Type = as.integer(sType), # Sense of Strand
 			chS = sChS, chE = sChE,
-			AAS = sAAS, idAAS = as.integer(sAA1),
-			AAE = sAAE, idAAE = as.integer(sAA2));
+			AAS = sAAS, idAAS = as.integer(sAASn), InsertS = sAASI,
+			AAE = sAAE, idAAE = as.integer(sAAEn), InsertE = sAAEI,
+			# Registration
+			atomRc   = atomRc,
+			insertRc = insertRc,
+			atomRp   = atomRp,
+			insertRp = insertRp
+			);
 	}
 	#
 	structMol = as.pdb.structure(helix, sheet);
